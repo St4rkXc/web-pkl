4de2b18a9cfaabd55e57e89a740a484d5c16515a
Stark <123723458+St4rkXc@users.noreply.github.com>
2024-12-04 00:06:45 +0800

    hero & section 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ["./*.html"],
+    theme: {
+        extend: {
+			colors: {
+				primary: "#25252B",
+				secondary: "#636376",
+				text: "#25252B",
+				success: "#75AD42",
+				succesbg: "#DDEBD0",
+				processbg: "#FFF4CC",
+				process: "#E5B300",
+			},
+		},
+        fontSize: {
+            xs: "14px",
+            base: "16px",
+            lg: "20px",
+            xl: "24px",
+            "2xl": "32px",
+            "3xl": "64px",
+            "4xl": "96px",
+        },
+    },
 
+    daisyui: {
+        themes: [
+            {
+                mytheme: {
+                    primary: "#25252B",
+                    secondary: "#636376",
+                    "base-100": "#EFEFEF" /* background image */,
+                },
+            },
+            "dark",
+        ],
+    },
+    plugins: [require("daisyui")],
+};
