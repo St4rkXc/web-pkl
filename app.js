ae4c77e49a6f8831811f69775eb8ed468b41b517
Stark <123723458+St4rkXc@users.noreply.github.com>
2024-12-06 01:19:59 +0800

    inner page proto & ui

--- app.js
+++ app.js
@@ -1,37 +1,39 @@
-const button1 = document.getElementById('but1');
-const button2 = document.getElementById('but2');
-const button3 = document.getElementById('but3');
-const content1 = document.getElementById('content1')
-const content2 = document.getElementById('content2')
-const content3 = document.getElementById('content3')
+const button1 = document.getElementById("but1");
+const button2 = document.getElementById("but2");
+const button3 = document.getElementById("but3");
+const content1 = document.getElementById("content1");
+const content2 = document.getElementById("content2");
+const content3 = document.getElementById("content3");
 
-console.log(button1)
-console.log(button2)
-console.log(button3)
-console.log(content1)
-console.log(content2)
-console.log(content3)
+console.log(button1);
+console.log(button2);
+console.log(button3);
+console.log(content1);
+console.log(content2);
+console.log(content3);
 
-function tabs(button, content ) {
-    // button1.classList.remove('border-b-primary/30')
-    button1.classList.remove('border-b-primary')
-    // button2.classList.remove('border-b-primary/30')
-    button2.classList.remove('border-b-primary')
-    // button3.classList.remove('border-b-primary/30')
-    button3.classList.remove('border-b-primary')
-    content1.classList.add('hidden')
-    content2.classList.add('hidden')
-    content3.classList.add('hidden')
+function tabs(button, secbutton, thirdbutton, content) {
+    // button1.classList.remove("border-b-primary/30");
+    button1.classList.remove("border-b-primary");
+    // button2.classList.remove("border-b-primary/30");
+    button2.classList.remove("border-b-primary");
+    // button3.classList.remove("border-b-primary/30");
+    button3.classList.remove("border-b-primary");
+    content1.classList.add("hidden");
+    content2.classList.add("hidden");
+    content3.classList.add("hidden");
 
-    button.classList.add('border-b-primary')
-    button.classList.remove('border-b-primary/30')
-    content.classList.add('flex')
-    content.classList.remove('hidden')
+    button.classList.add("border-b-primary");
+    secbutton.classList.add("border-b-primary/30");
+    thirdbutton.classList.add("border-b-primary/30");
+    content.classList.add("flex");
+    content.classList.remove("hidden");
+    button.classList.remove("border-b-primary/30");
 }
 
 // document.addEventListener('DOMContentLoaded', ()=> {
 //     button1.classList.add('border-b-primary/30')
 // })
-button1.addEventListener('click', ()=> tabs(button1, content1))
-button2.addEventListener('click', ()=> tabs(button2, content2))
-button3.addEventListener('click', ()=> tabs(button3, content3))
+button1.addEventListener("click", () => tabs(button1, button2, button3, content1));
+button2.addEventListener("click", () => tabs(button2, button1, button2, content2));
+button3.addEventListener("click", () => tabs(button3, button2, button3, content3));
